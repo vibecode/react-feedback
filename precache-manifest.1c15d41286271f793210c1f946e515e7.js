6110d9b49672c89fcfa2a812a000165e56769c04
vibecode <rumashkin@gmail.com>
2019-02-20 22:27:02 +0100

    Updates

---
+++ precache-manifest.1c15d41286271f793210c1f946e515e7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "cac3b6c848bb1c54a2f7",
+    "url": "/react-feedback/static/css/main.9cc12404.chunk.css"
+  },
+  {
+    "revision": "cac3b6c848bb1c54a2f7",
+    "url": "/react-feedback/static/js/main.cac3b6c8.chunk.js"
+  },
+  {
+    "revision": "96f33c232e9075c2ddf3",
+    "url": "/react-feedback/static/css/1.0e052fe5.chunk.css"
+  },
+  {
+    "revision": "96f33c232e9075c2ddf3",
+    "url": "/react-feedback/static/js/1.96f33c23.chunk.js"
+  },
+  {
+    "revision": "7bf6756fcbf9a60a4bec",
+    "url": "/react-feedback/static/js/runtime~main.7bf6756f.js"
+  },
+  {
+    "revision": "38d68dbe68804229123f273f12a797de",
+    "url": "/react-feedback/static/media/bg.38d68dbe.jpg"
+  },
+  {
+    "revision": "889561b9d19eb4f3a3f00dd098d6ab29",
+    "url": "/react-feedback/static/media/bowl.889561b9.svg"
+  },
+  {
+    "revision": "135608d746ff344154293e04a61a19ee",
+    "url": "/react-feedback/static/media/star.135608d7.svg"
+  },
+  {
+    "revision": "1101649eddf599edea719b1107ec6cf8",
+    "url": "/react-feedback/static/media/thumb.1101649e.svg"
+  },
+  {
+    "revision": "058791a40962d50a8bc499d344959d3d",
+    "url": "/react-feedback/static/media/pen.058791a4.svg"
+  },
+  {
+    "revision": "8ce44aa2db4dd69cd7ebc190fb67bd13",
+    "url": "/react-feedback/static/media/lightning.8ce44aa2.svg"
+  },
+  {
+    "revision": "0431f9958dea213b9bc4f6d6149b6412",
+    "url": "/react-feedback/static/media/heart.0431f995.svg"
+  },
+  {
+    "revision": "2cc81c58e55a832590eef0da6a62725f",
+    "url": "/react-feedback/static/media/check.2cc81c58.svg"
+  },
+  {
+    "revision": "8799e9ca11f6059351485cd54f884618",
+    "url": "/react-feedback/index.html"
+  }
+];
